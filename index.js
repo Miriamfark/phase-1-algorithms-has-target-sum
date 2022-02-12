16db22c2d7b9794437d55a811148202506ed8d20
Miriam Farkas <miriamjoshua00@gmail.com>
2022-02-12 22:47:20 +0200

    commit tree ff944a0eb47d0be53749bc57635e48fccd6e760a

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  if ((array[0] + array[1]) === target) {
+    return true
+  } else if ((array[0]+array[2]) === target) {
+    return true
+  } else if((array[0]+array[3])=== target) {
+    return true
+  } else if((array[0]+array[4])=== target) {
+    return true
+  } else if((array[0]+array[5])=== target) {
+    return true
+  } else if((array[1]+array[2])=== target) {
+    return true
+  } else if((array[1]+array[3])=== target) {
+    return true
+  } else if((array[1]+array[4])=== target) {
+    return true
+  } else if((array[1]+array[5])=== target) {
+    return true
+  } else if((array[2]+array[3])=== target) {
+    return true
+  } else if((array[2]+array[4])=== target) {
+    return true
+  } else if((array[2]+array[5])=== target) {
+    return true
+  } else if((array[3]+array[4])=== target) {
+    return true
+  } else if((array[3]+array[5])=== target) {
+    return true
+  } else if((array[4]+array[5])=== target) {
+    return true
+  }
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -17,6 +48,8 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
